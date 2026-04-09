1820931e6d55d47d0a8fad9964192974c9f88718
lavakushapolasa <151919527+lavakushapolasa@users.noreply.github.com>
2026-04-09 14:51:32 +0530

    Delete 01_Time_Converter.py

--- 01_Time_Converter.py
+++
@@ -1,9 +0,0 @@
-minutes = int(input("Enter minutes: "))
-
-hours = minutes // 60
-mins = minutes % 60
-
-if hours > 0:
-    print(f"{hours} hr{'s' if hours > 1 else ''} {mins} minutes")
-else:
-    print(f"{mins} minutes")
